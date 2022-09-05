437dbb3d860a3cb2c89a96ea2b8f5df353ed4636
Andrey Kravchenko <andreykravchenkodeveloper@gmail.com>
2022-09-05 16:19:55 +0300

    add level 3-4

---
+++ src/test/java/project/dao/LibraryDAOTest.java
@@ -0,0 +1,50 @@
+package project.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import project.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryDAOTest {
+    private LibraryDAO libraryDAO;
+
+    @BeforeEach
+    void setUp() {
+        libraryDAO = new LibraryDAO();
+    }
+
+    @Test
+    void borrowBookIdToReaderId() {
+    }
+
+    @Test
+    void returnBookIdFromReaderId() {
+    }
+
+    @Test
+    void findAllBorrowedBooksByReaderId() {
+        List<Book> bookList = new ArrayList<>();
+
+        Book bookFirst = new Book(4, "No Brakes: My Top Gear Years", "Jeremy Clarkson");
+        Book bookSecond = new Book(7, "Season of Storms", "Andrzej Sapkowski");
+        Book bookThree = new Book(8, "Women", "Charles Bukowski");
+
+        bookList.add(bookFirst);
+        bookList.add(bookSecond);
+        bookList.add(bookThree);
+
+        assertEquals(libraryDAO.findAllBorrowedBooksByReaderId(3), bookList);
+    }
+
+    @Test
+    void findAllReadersByBookId() {
+    }
+
+    @Test
+    void findAllReadersAndBorrowedBooks() {
+    }
+}
